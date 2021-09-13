6781c2f80ffe7fbd0485c621c39a3893d0e5d2e1
Keith Parks <keithparks20@gmail.com>
2021-09-13 15:49:23 -0500

    added home module that will list stocks. also added the route to Application Views

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { Home } from "./home/home"
 
 export const ApplicationViews = () => {
     return (
         <>
             <Route path="/home">
-                
+                <Home />
             </Route>
 
             <Route path="/watchlist">
