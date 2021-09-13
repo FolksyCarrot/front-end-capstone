67fe0879f0ffba9949416f99f962dc6c9598dd61
Keith Parks <keithparks20@gmail.com>
2021-09-13 13:58:19 -0500

    added Appication views module for routing. skeletal framework only

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            <Route path="/home">
+                
+            </Route>
+
+            <Route path="/watchlist">
+                
+            </Route>
+
+            <Route path="/stockForm">
+                
+            </Route>
+
+        
+        </>
+    )
+}
