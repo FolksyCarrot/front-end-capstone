1c46714a44dbec3f291ef5e5b8ba388c4ca625ef
Keith Parks <keithparks20@gmail.com>
2021-09-13 20:05:38 -0500

    got the JSX on the home page to render

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react"
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom"
-import "./Login.css"
+// import "./Login.css"
 
 export const Login = () => {
     const [email, set] = useState("")
@@ -20,7 +20,7 @@ export const Login = () => {
             .then(exists => {
                 if (exists) {
                     localStorage.setItem("stock_customer", exists.id)
-                    history.push("/")
+                    history.push("/home")
                 } else {
                     existDialog.current.showModal()
                 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -9,7 +9,8 @@ export const Home = () => {
         fetch ("http://api.marketstack.com/v1/eod?access_key=7932502e72211a0b7cec01fa7a53f0e3&symbols=AAPL,MSFT,BA,C,JPM,PFE,DIS,GM,QCOM,CAT,WFC,IBM,NVDA,LMT,SNAP,SBUX,AMD,BABA,SNAP,TSLA,BAC&limit=20")
             .then ( res => res.json())
             .then ((data)=> {
-                setStocks(data)
+                setStocks(data.data)
+                console.log(data.data)
             })
     }
 
@@ -25,11 +26,12 @@ export const Home = () => {
     return (
         <>
             <h1> Soaring Stocks</h1>
+           
                 <ul>
 
                 {
                     stocks.map((stock) => {
-                        return <li key={stock.data.id}>{stock.data.symbol} {stock.data.open} {stock.data.volume}<button onClick={() => history.push("/")}>Add to watchlist</button></li>
+                        return <li key={stock.symbol}>{stock.symbol} {stock.open} {stock.volume}<button >Add to watchlist</button></li>
                     })
                 }
 
