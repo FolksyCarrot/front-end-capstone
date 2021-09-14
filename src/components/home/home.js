c6e9ff880bd9c2edc59207a67746862f38de7b6e
Keith Parks <keithparks20@gmail.com>
2021-09-14 13:23:49 -0500

    updated the stockfrom to take user input and then post to watchlist array in local API database

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState} from "react"
-
+import { useHistory } from "react-router-dom";
 
 export const Home = () => {
 
     const [stocks, setStocks] = useState([])
-
+    const history = useHistory()
     const reRender = () => {
         fetch ("http://api.marketstack.com/v1/eod?access_key=7932502e72211a0b7cec01fa7a53f0e3&symbols=AAPL,MSFT,BA,C,JPM,PFE,DIS,GM,QCOM,CAT,WFC,IBM,NVDA,LMT,SNAP,SBUX,AMD,BABA,SNAP,TSLA,BAC&limit=20")
             .then ( res => res.json())
             .then ((data)=> {
                 setStocks(data.data)
-                console.log(data.data)
             })
     }
 
@@ -31,7 +30,7 @@ export const Home = () => {
 
                 {
                     stocks.map((stock) => {
-                        return <li key={stock.symbol}>{stock.symbol} Open:{stock.open} Volume:{stock.volume}<button >Add to watchlist</button></li>
+                        return <li key={stock.symbol}>{stock.symbol} Open:{stock.open} Volume:{stock.volume}<button onClick={() => history.push(`/stockform/${stock.symbol}`)} >Add to watchlist</button></li>
                     })
                 }
 
