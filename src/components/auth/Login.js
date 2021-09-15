3f029366e4aa4086e90a91a9d8b3971ba8ab6408
Keith Parks <keithparks20@gmail.com>
2021-09-15 14:44:57 -0500

    added a background image and changed the styling to the login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react"
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom"
-// import "./Login.css"
+import "./Login.css"
 
 export const Login = () => {
     const [email, set] = useState("")
