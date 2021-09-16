81d3c9613f673073d108ae44403b13ec34f0b75b
Keith Parks <keithparks20@gmail.com>
2021-09-16 09:22:18 -0500

    updated the individual stocks to have cards. Assigned a seperate css module strictly for the home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState} from "react"
 import { useHistory } from "react-router-dom";
+import "./home.css"
 
 export const Home = () => {
 
@@ -33,7 +34,7 @@ export const Home = () => {
                 {
                     stocks.map((stock) => {
                         
-                        return <li key={stock.symbol}><h2>{stock.name}</h2><h3>Ticker: {stock.symbol}</h3> <h5>Open: {stock.open}</h5> <h5> Close: {stock.close}</h5> <h5>Volume: {stock.volume}</h5><button onClick={() => history.push(`/stockform/${stock.symbol}`)} >Add to watchlist</button></li>
+                        return <div className="stock--list" ><li className="modal" key={stock.symbol}><h2>{stock.name}</h2><h3>Ticker: {stock.symbol}</h3> <h5>Open: {stock.open}</h5> <h5> Close: {stock.close}</h5> <h5>Volume: {stock.volume}</h5><button onClick={() => history.push(`/stockform/${stock.symbol}`)} >Add to watchlist</button></li></div>
                         
                     })
                 }
@@ -45,3 +46,5 @@ export const Home = () => {
 }
 
 // http://api.marketstack.com/v1/eod?access_key=7932502e72211a0b7cec01fa7a53f0e3&symbols=AAPL,MSFT,BA,C,JPM,PFE,DIS,GM,QCOM,CAT,WFC,IBM,NVDA,LMT,SNAP,SBUX,AMD,BABA,SNAP,TSLA,BAC&limit=20
+
+//added div of modaul to the return statement
